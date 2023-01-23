1b34c9a46ceb62d1196660d0e1e5fc3fb243023d
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-01-23 17:55:14 -0500

    Agregando cv

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,36 +1,36 @@
-let id=0;
-const Carrusel= {
-    'proy1':{
-        'img':'./img/proyGymAfit.JPG',
-        'title':'Afit gym',
-        'link':'./'
+let id = 0;
+const Carrusel = {
+    'proy1': {
+        'img': './img/proyGymAfit.JPG',
+        'title': 'Afit gym (Front End)',
+        'link': 'https://edwardmelendezm.github.io/'
     },
-    'proy2':{
-        'img':'./img/proyCasaMiau.JPG',
-        'title':'Casa Miau',
-        'link':'./'
+    'proy2': {
+        'img': './img/proyCasaMiau.JPG',
+        'title': 'Casa Miau (Back end - Front End)',
+        'link': 'https://github.com/EdwardMelendezM/CasaMiau'
     },
-    'proy3':{
-        'img':'./img/proyCentroComputo.JPG',
-        'title':'Control asistencia y silabus Unsaac',
-        'link':'./'
+    'proy3': {
+        'img': './img/proyCentroComputo.JPG',
+        'title': 'Sistema gestor de silabos y asistencia de docente (Back end - Front End)',
+        'link': 'https://github.com/LosCuatroFantasticoss/ProyectoIngenieriaSoftware2022-II'
     }
 }
 
 
-let button_atras=d.getElementById("button_atras"),
-button_siguiente=d.getElementById("button_siguiente");
-let espacio_proyecto=d.getElementById("espacio_proyecto");
+let button_atras = d.getElementById("button_atras"),
+    button_siguiente = d.getElementById("button_siguiente");
+let espacio_proyecto = d.getElementById("espacio_proyecto");
 
 
-function carrusel_inicio(proy){
-    if(espacio_proyecto.dataset.value='0'){
-        insertar_proyecto(Carrusel.proy1,espacio_proyecto);
-        espacio_proyecto.dataset.value='1'
+function carrusel_inicio(proy) {
+    if (espacio_proyecto.dataset.value = '0') {
+        insertar_proyecto(Carrusel.proy1, espacio_proyecto);
+        espacio_proyecto.dataset.value = '1'
     }
 }
 
-function insertar_proyecto(proy,espacio,movimiento){
+function insertar_proyecto(proy, espacio, movimiento) {
     const template = `
     <div class="proy ${movimiento}">
         <a href="${proy.link}">
@@ -41,43 +41,43 @@ function insertar_proyecto(proy,espacio,movimiento){
     `;
     espacio.innerHTML = template;
 }
-function carrusel(e){
-    if(e.target==button_atras){
+function carrusel(e) {
+    if (e.target == button_atras) {
         console.log("Button atras...")
-        let valor=espacio_proyecto.dataset.value;
+        let valor = espacio_proyecto.dataset.value;
         switch (valor) {
             case '1':
-                insertar_proyecto(Carrusel.proy3,espacio_proyecto,'mostrarIzquierda');
-                espacio_proyecto.dataset.value='3'
+                insertar_proyecto(Carrusel.proy3, espacio_proyecto, 'mostrarIzquierda');
+                espacio_proyecto.dataset.value = '3'
                 break;
             case '2':
-                insertar_proyecto(Carrusel.proy1,espacio_proyecto,'mostrarIzquierda');
-                espacio_proyecto.dataset.value='1'
+                insertar_proyecto(Carrusel.proy1, espacio_proyecto, 'mostrarIzquierda');
+                espacio_proyecto.dataset.value = '1'
                 break;
             case '3':
-                insertar_proyecto(Carrusel.proy2,espacio_proyecto,'mostrarIzquierda');
-                espacio_proyecto.dataset.value='2'
+                insertar_proyecto(Carrusel.proy2, espacio_proyecto, 'mostrarIzquierda');
+                espacio_proyecto.dataset.value = '2'
                 break;
             default:
                 console.error('Algo ha ocurrido...');
                 break;
         }
     }
-    if(e.target==button_siguiente){
+    if (e.target == button_siguiente) {
         console.log("BOTTON Adelante...");
-        let valor=espacio_proyecto.dataset.value;
+        let valor = espacio_proyecto.dataset.value;
         switch (valor) {
             case '1':
-                insertar_proyecto(Carrusel.proy2,espacio_proyecto,'mostrarDerecha');
-                espacio_proyecto.dataset.value='2'
+                insertar_proyecto(Carrusel.proy2, espacio_proyecto, 'mostrarDerecha');
+                espacio_proyecto.dataset.value = '2'
                 break;
             case '2':
-                insertar_proyecto(Carrusel.proy3,espacio_proyecto,'mostrarDerecha');
-                espacio_proyecto.dataset.value='3'
+                insertar_proyecto(Carrusel.proy3, espacio_proyecto, 'mostrarDerecha');
+                espacio_proyecto.dataset.value = '3'
                 break;
             case '3':
-                insertar_proyecto(Carrusel.proy1,espacio_proyecto,'mostrarDerecha');
-                espacio_proyecto.dataset.value='1'
+                insertar_proyecto(Carrusel.proy1, espacio_proyecto, 'mostrarDerecha');
+                espacio_proyecto.dataset.value = '1'
                 break;
             default:
                 console.error('Algo ha ocurrido...');
@@ -85,5 +85,5 @@ function carrusel(e){
         }
     }
 }
-window.addEventListener("DOMContentLoaded",carrusel_inicio);
-window.addEventListener("click",carrusel);
+window.addEventListener("DOMContentLoaded", carrusel_inicio);
+window.addEventListener("click", carrusel);
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Portafolio personal
 [![readme.jpg](https://i.postimg.cc/cLTx0Q3b/readme.jpg)](https://postimg.cc/zybZkH0C)
 # Aun en desarrollo
+    
